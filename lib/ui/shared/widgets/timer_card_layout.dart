abc75166a78facaa333e25c7ea49a3c19d5123f0
Leandro Mancini de Sousa <mancinilele@gmail.com>
2021-05-27 21:40:31 -0300

    feat(widget): create widget timer card

---
+++ lib/ui/shared/widgets/timer_card_layout.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:timepad/ui/shared/styles.dart';
+
+class TimerCardLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: dsBlack,
+      margin: EdgeInsets.symmetric(
+        horizontal: dsMedium
+      ),
+      child: InkWell(
+        onTap: () => { print('click card') },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(dsXSmall))
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: dsMedium,
+            vertical: ds2XLarge
+          ),
+          child: Column(
+            children: [
+              timeContent(),
+              taskContent(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget timeContent() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '00:32:10',
+          style: dsHeadingPetaWhite,
+        ),
+        Icon(
+          Icons.arrow_forward_ios,
+          color: dsWhite,
+          size: dsMedium,
+        )
+      ],
+    );
+  }
+
+  Widget taskContent() {
+    return Row(
+      children: [
+        Icon(
+          Icons.timer,
+          color: dsWhite,
+          size: dsMedium,
+        ),
+        SizedBox(
+          width: dsXSmall,
+        ),
+        Text(
+          'Design System',
+          style: dsTextGigaWhite,
+        )
+      ],
+    );
+  }
+}
--- lib/ui/tasks/timer_card_view.dart
+++ lib/ui/tasks/timer_card_view.dart
@@ -1,75 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:timepad/ui/shared/styles.dart';
+import 'package:timepad/ui/shared/widgets/timer_card_layout.dart';
 
 class TimerCardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
-      child: cardContent(),
-    );
-  }
-
-  Widget cardContent() {
-    return Card(
-      elevation: 0,
-      color: dsBlack,
-      margin: EdgeInsets.symmetric(
-        horizontal: dsMedium
-      ),
-      child: InkWell(
-        onTap: () => { print('click card') },
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(dsXSmall))
-          ),
-          padding: EdgeInsets.symmetric(
-            horizontal: dsMedium,
-            vertical: ds2XLarge
-          ),
-          child: Column(
-            children: [
-              timeContent(),
-              taskContent(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget timeContent() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          '00:32:10',
-          style: dsHeadingPetaWhite,
-        ),
-        Icon(
-          Icons.arrow_forward_ios,
-          color: dsWhite,
-          size: dsMedium,
-        )
-      ],
-    );
-  }
-
-  Widget taskContent() {
-    return Row(
-      children: [
-        Icon(
-          Icons.timer,
-          color: dsWhite,
-          size: dsMedium,
-        ),
-        SizedBox(
-          width: dsXSmall,
-        ),
-        Text(
-          'Design System',
-          style: dsTextGigaWhite,
-        )
-      ],
+      child: TimerCardLayout(),
     );
   }
 }
